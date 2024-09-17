061c558b8b794e8ef2e76bc4ef487de592ace947
Sagar Singh <sagarnegi926@gmail.com>
2024-09-17 19:37:26 +0530

    add changes

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const SignUp = require('./models/signup.models');
+
+const app = express();
+app.use(cors());
+
+const dburi = "mongodb+srv://sagarnegi926:UuDO4kMGzVffjD0u@cluster0.4lefr.mongodb.net/";
+const connectDb = async () => {
+    try {
+        await mongoose.connect(dburi);
+        console.log("connected successfully");
+    } catch (error) {
+        console.error("Error : ",error)
+    }
+} 
+
+connectDb();
+
+app.post('/signup', async(req, res) => {
+    const {name , email , password} = req.body();
+    try {
+        const newuser = new SignUp({name , email, password});
+        await newuser.save();
+        res.status(201).json({message : "user created successfully"})
+    } catch (error) {
+        console.error("Error : ",error);
+    }
+})
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`app running on ${port}`);
+})
+
---
+++ backend/models/signup.models.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const signupSchema = mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true
+        },
+        email : {
+            type : String,
+            required : true
+        },
+        password : {
+            type : String,
+            required : true
+        }
+
+    }
+)
+
+const Signup = mongoose.model('Signup', signupSchema);
+
+module.exports = Signup;
